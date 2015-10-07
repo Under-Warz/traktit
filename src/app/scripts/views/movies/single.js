ca5bcbe2f8872bf2a4cc3551032a9d20542afdf9
Laurent Menu <menu.laurent@gmail.com>
2015-10-07 14:52:12 +0200

    Add movie toolbar + related movies

--- src/app/scripts/views/movies/single.js
+++ src/app/scripts/views/movies/single.js
@@ -3,13 +3,13 @@ var App = require('App');
 
 module.exports = ChildItemView.extend({
     template: require('../../templates/movies/single.hbs'),
+    castSwiper: null,
+    relatedSwiper: null,
 
     initialize: function() {
         // Load movie
         if (this.model.get('fetched') == false) {
-            this.model.fetch(function() {
-                console.log('fetched !!!');
-            });
+            this.model.fetch();
         }
 
         // Reload
@@ -20,5 +20,53 @@ module.exports = ChildItemView.extend({
         _.each(this.$el.find('#movies-single *[data-attr]'), _.bind(function(elm) {
             $(elm).empty().html(this.model.get($(elm).data('attr')));
         }, this));
+
+        // Render actors
+        this.renderCast();
+
+        // Render related
+        this.renderRelated();
+    },
+
+    onShow: function() {
+        ChildItemView.prototype.onShow.call(this, this, null, true);
+
+        // Render actors
+        this.renderCast();
+
+        // Render related
+        this.renderRelated();
+    },
+
+    renderCast: function() {
+        try {
+            if (this.model.get('cast').length > 0 && this.castSwiper == null) {
+                this.castSwiper = window.f7.swiper(this.$el.find('#movies-single .cast'), {
+                    slidesPerView: 'auto'
+                });
+
+                _.each(this.model.get('cast'), _.bind(function(item) {
+                    this.castSwiper.appendSlide(require('../../templates/partials/actor.hbs')(item));
+                }, this)); 
+            }
+        }
+        catch(e) {        
+        }
+    },
+
+    renderRelated: function() {
+        try {
+            if (this.model.get('related').length > 0 && this.relatedSwiper == null) {
+                this.relatedSwiper = window.f7.swiper(this.$el.find('#movies-single .related'), {
+                    slidesPerView: 'auto'
+                });
+
+                _.each(this.model.get('related'), _.bind(function(item) {
+                    this.relatedSwiper.appendSlide(require('../../templates/partials/movie.hbs')(item));
+                }, this)); 
+            }
+        }
+        catch(e) {        
+        }
     }
 });
